2fe591d948729c4cde10869795b3ad849ad55767
jonghyeonjo99 <hyeon058@naver.com>
2023-04-16 23:40:37 +0900

    배포파일

--- pill_info_2(git)/src/main/java/com/example/pill_info_2/repository/mediinfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pill_info_2.repository;
-
-import com.example.pill_info_2.model.mediinfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface mediinfoRepository extends JpaRepository <mediinfo, Long> {
-}
--- pillinfo(git)/src/main/java/com/example/pillinfo/repository/PillInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pillinfo.repository;
-
-import com.example.pillinfo.Pill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PillInfoRepository extends JpaRepository<Pill,Long> {
-}
